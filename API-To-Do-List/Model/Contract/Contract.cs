ff85d93c1ebeaeb6cd2bde811ff192a76805d7b4
unknown <mangussinicholas@gmail.com>
2024-12-20 11:41:23 -0300

    Model

---
+++ API-To-Do-List/Model/Contract/Contract.cs
@@ -0,0 +1,30 @@
+﻿using API_To_Do_List.Model.Cliente;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_To_Do_List.Model.Contrato
+{
+    public class Contract
+    {
+        [Key]
+        private int _id { get; set; }
+        private string _contract_name { get; set; }
+        private DateOnly _date_hiring { get; set; }
+        private string _contract_type { get;set }
+        private bool _active { get; set; }
+        [ForeignKey("customer_id")]
+        private Customer customer { get; set; }
+
+        public Contract(int id, string contract_name, DateOnly date_hiring, string contract_type, bool active)
+        {
+            _id = id;
+            _contract_name = contract_name;
+            _date_hiring = date_hiring;
+            _contract_type = contract_type;
+            _active = active;
+        }
+
+        public Contract() { }
+
+    }
+}
---
+++ API-To-Do-List/Model/Customer/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_To_Do_List.Model.Cliente
+{
+    public class Customer
+    {
+        [Key]
+        private int _id { get; set; };
+        private int _enterprise_registration { get; set; };
+        private string _name { get; set; };
+        private string _commercial_address { get; set; };
+        private DateOnly _opening_date { get; set; };
+        private string _sector { get; set; };
+        private bool _is_open { get; set; };
+
+        public Customer() { }
+        public Customer(int id, int enterprise_registration, string name, string commercial_address, DateOnly opening_date, string sector, bool is_open)
+        {
+            _id = id;
+            _enterprise_registration = enterprise_registration;
+            _name = name;
+            _commercial_address = commercial_address;
+            _opening_date = opening_date;
+            _sector = sector;
+            _is_open = is_open;
+        }
+
+
+        
+
+
+    }
+}
